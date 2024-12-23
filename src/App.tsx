9bca413820d0efb0af9f9eff7f870a484817479c
GBlackwell1 <gabrielsblackwell@gmail.com>
2024-12-23 00:11:37 -0500

    initial look

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,30 @@
 import React, {useState, useEffect } from 'react';
 import './App.css';
-import Login from './Login';
-import Home from './components/Home';
-import firebase from './firebaseConfig/firebaseConfig';
-
-
-// TODO: Import home 
-// TODO: Add HI3 Tech Logo
-
+import UI from './components/UI';
 
 const App: React.FC = () => {  // React.FC <= function component
-  /* Initialize all states needed */
-  const [isUser, setIsUser] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [error, setError] = useState<string>('');
-  // const [hasAccount, setHasAccount] = useState<boolean>(true);
-  // Function to clear input fields in login
-  function clearInputs() {
-    setEmail('');
-    setPassword('');
-  }
-  // Function to clear error fields in login
-  function clearErrors() {
-    setError('');
-  }
-  // Handle the login of the user
-  // TODO: Maybe automate email to me for if user not registered?
-  // TODO: Custom error message current looks weird
-  function handleLogin() {
-    clearErrors();
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch(err => { setError(err.message); });
-    /* IDK if this is correct but I think of the above as a lambda function in C++???
-       Well I know this is a try catch statement but in a function 
-      but the whole thingamabob with the {} */
-  }
-  // Handle logout for the user
-  function handleLogout() {
-    firebase.auth().signOut();
-  }
-  // Listen to see if the authentication state has changed
-  function authListener() {
-    firebase.auth().onAuthStateChanged( user => {
-      if(user) {
-        clearInputs();
-        setIsUser(true);  // FIXME: Lets hope this works
-      } else 
-        setIsUser(false);
-    })
-  }
-  // Not sure what this really does yet
-  useEffect(() => {
-    handleLogout();
-    authListener();  // FIXME: Something wrong here?
-  },[]);
+  const [open, setOpen] = useState<boolean>(false);
+  // TODO: Make a list of all the elements in the board
+  // TODO: Make sure the drawer opening is cohesive with the logo __board
+  // TODO: Make sure individual elements support click and drag to additional elevations
+  // TODO: Partition UI 
+  // TODO: Map sections
 
-  return (
-    <div className="App">
-      { isUser ? (
-        <Home handleLogout={handleLogout} />
-      ):(
-        <Login 
-        email = {email}
-        setEmail = {setEmail}
-        password = {password}
-        setPassword = {setPassword}
-        handleLogin = {handleLogin}
-        // hasAccount = {hasAccount}
-        // setHasAccount = {setHasAccount}
-        error = {error}
-        /> 
-      )}
-    
+  /* Thoughts:
+  * - Parent object for all widgets, will control API request rate (global settings instead?)
+  * - Unsure of how to handle drag feature however for future, continuously divide the sections into halves based on the half in which the cursor is hovered in
+  */
+  return ( 
+    <div>
+      <UI />
+      <div className="draggableParent">
+        <div className="draggableSection">DraggableSection</div>
+        <div className="draggableSection">DraggableSection</div>
+        <div className="draggableSection">DraggableSection</div>
+        <div className="draggableSection">DraggableSection</div>
+      </div>
+ 
     </div>
   );
 }
-
 export default App;
