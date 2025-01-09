08ba184d188677a99f3df1edf59413bcaa9c5595
GBlackwell1 <gabrielsblackwell@gmail.com>
2025-01-08 19:17:27 -0500

    start logic for x and y pos for widget dropping

--- src/components/DropSection.tsx
+++ src/components/DropSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import "../App.css";
 import { RootState } from '../redux/reduxTypes';
@@ -7,22 +7,45 @@ const selectState = (state: RootState) => state.itemSelected.itemSelected;
 type Props = { id: string; };
 
 const DropSection: React.FC<Props> = ({id}) => {
+    const [elementState, setElementState] = useState<number>(0);
     const [mouseEnter, setMouseEnter] = useState<boolean>(false);
+    const [mousePos, setMousePos] = useState<{x: number, y: number}>({x: 0, y: 0});
     const itemSelected = useSelector(selectState);
-    let sectionRef = document?.getElementById(`${id}-section`);
-    const parentElt = document?.getElementById("toplevel");
+    
 
-    sectionRef?.addEventListener("mouseup", () => {
-        if (sectionRef && parentElt) {
-            let parentPosition = parentElt.getBoundingClientRect();
-            let sectionPosition = sectionRef.getBoundingClientRect();
-            //let sectionWidth = window.getComputedStyle(sectionRef).width;
-            
-            
-            if (mouseEnter && itemSelected)  
-                sectionRef.innerHTML = itemSelected;
+    useEffect(() => {
+        const sectionRef = document?.getElementById(`${id}-section`);
+        const parentElt = document?.getElementById("toplevel");
+
+        function handleMouseMovement(event: MouseEvent) {
+            setMousePos({x: event.clientX, y: event.clientY});
+        }   
+
+        function handleMouseUp() {
+            if (sectionRef && parentElt) {
+                let sectionPosition = sectionRef.getBoundingClientRect();            
+                if (mouseEnter && itemSelected)  {
+                    // TODO: Logic for determining movement of widget sections
+                    // Probably need a switch case statement here
+                    if ((mousePos.x-sectionPosition.x)-sectionPosition.width/2 < 0)
+                        console.log("Dropped "+itemSelected+" on left");
+                    else
+                        console.log("Dropped "+itemSelected+" on right");
+                    setElementState(elementState + 1);
+                    sectionRef.innerHTML = itemSelected;
+                }
+            }
         }
-    });
+
+        window.addEventListener("mousemove", handleMouseMovement);
+        sectionRef?.addEventListener("mouseup", handleMouseUp);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMovement);
+            sectionRef?.removeEventListener("mouseup", handleMouseUp);
+        }
+
+    }, [mouseEnter, mousePos, itemSelected, elementState, id]);
 
     return (
         <div 
